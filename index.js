c846c0750287bb1f3fe5746389a3fa2974930392
ashutosh043 <ashusah043@gmail.com>
2025-08-03 19:31:21 +0530

    test

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
 app.set('view engine', 'ejs');
-const router = require('./controller/nijivsha');
-require('./config/db');
-app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-       
+const router = require('./controller/nijivsha')
+
 app.use(express.static(path.join(__dirname, '/public')))
-app.use(router);
+app.use(router)
+
+
+
+
 
 app.listen(port);
